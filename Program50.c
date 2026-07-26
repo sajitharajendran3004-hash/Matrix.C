9f8290318156741d7beeeaf116d0f58b3ddf34ee
sajitharajendran3004-hash <sajitharajendran3004@gmail.com>
2026-07-26 16:09:07 +0530

    Create Program50.c

---
+++ Program50.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Inverse of a 3x3 Matrix is an advanced program.\n");
+    printf("It requires determinant, cofactor and adjoint calculations.");
+
+    return 0;
+}
