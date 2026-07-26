a0d5b66fd373cb88e605ca134182c9a2eee9bf43
sajitharajendran3004-hash <sajitharajendran3004@gmail.com>
2026-07-26 15:57:18 +0530

    Add program to check if a matrix is symmetric

---
+++ Program25.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], n, i, j, flag = 1;
+
+    printf("Enter order of matrix: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+        for(j = 0; j < n; j++)
+            scanf("%d", &a[i][j]);
+
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            if(a[i][j] != a[j][i])
+                flag = 0;
+        }
+    }
+
+    if(flag)
+        printf("Symmetric Matrix");
+    else
+        printf("Not a Symmetric Matrix");
+
+    return 0;
+}
