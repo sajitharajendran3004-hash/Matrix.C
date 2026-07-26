3c6f67817c85c372d7b9c9645174d2bce2e1e3a7
sajitharajendran3004-hash <sajitharajendran3004@gmail.com>
2026-07-26 16:03:25 +0530

    Create Program38.c

---
+++ Program38.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    int a[2][2], det;
+
+    printf("Enter 2x2 matrix:\n");
+
+    for(int i=0;i<2;i++)
+        for(int j=0;j<2;j++)
+            scanf("%d",&a[i][j]);
+
+    det = (a[0][0]*a[1][1]) - (a[0][1]*a[1][0]);
+
+    printf("Determinant = %d",det);
+
+    return 0;
+}
